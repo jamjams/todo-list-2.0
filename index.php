1b39e4faf0164a793ec0bf625ce5dfad451e010a
Jahmelah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-04-23 10:43:58 -0700

    milestone 2

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-index.php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple To-do list</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+
+</body>
+</html>
